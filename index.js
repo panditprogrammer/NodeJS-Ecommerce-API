128d1e18e60151aecd47290176c86e1e8151b5b9
panditprogrammer <panditprogrammer@gmail.com>
2023-10-18 19:38:45 +0530

    express server running

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-https://github.com/panditprogrammer/NodeJS-Ecommerce-API.git
+const express = require("express");
+const server = express();
+const port = 3000;
+
+
+server.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
+server.listen(port,()=>{
+    console.log(`server is running http://localhost:${port}`);
+});
