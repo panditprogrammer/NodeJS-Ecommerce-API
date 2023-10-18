6ccec1065dfa2b91efaa6907f2d8cb9c26cd2bd3
panditprogrammer <panditprogrammer@gmail.com>
2023-10-18 21:53:23 +0530

    body-parser setup for json

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const server = express();
 
 require("dotenv/config");
 const port = process.env.PORT;
+const api_url = process.env.API_URL;
+
+// middleware for json 
+const bodyParser = require("body-parser");
+server.use(bodyParser.json());
 
 
-server.get("/",(req,res)=>{
-    res.send("Hello World");
+
+server.post(api_url+"/products",(req,res)=>{
+    let product = req.body;
+    console.log(product);
+    res.send(product);
 });
 
+
+
+
+
 server.listen(port,()=>{
-    console.log(`server is running http://localhost:${port}`);
+    console.log(`server is running http://localhost:${port}${api_url}`);
 });
