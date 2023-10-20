c51490527db1c2ad136c7b87faed90435b1c947e
panditprogrammer <panditprogrammer@gmail.com>
2023-10-20 18:13:33 +0530

    Product Schema done

--- models/product.js
+++ models/product.js
@@ -5,13 +5,53 @@ const mongoose = require("mongoose");
 
 // create schemas for product 
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    price: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    richDescription:{
+        type: String,
+        default: null
+    },
+    image:[{
+        type:String
+    }],
+    price: {
+        type: Number,
+        default: 0
+    },
+    category:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
     stock: {
         type: Number,
-        required: true
+        required: true,
+        min: 0,
+        max: 255
+    },
+    rating:{
+        type: Number,
+        default: 0,
+    },
+    reviews:{
+        type: Number,
+        default: 0,
+    },
+    featured:{
+        type:Boolean,
+        default: false
+    },
+    created_on:{
+        type: Date,
+        default: Date.now
     }
+
 });
 
 // create Collection in mongoDB 
