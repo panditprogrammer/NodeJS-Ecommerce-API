5d7ada9c084d0add30966d75a47beb2d1725aff5
panditprogrammer <panditprogrammer@gmail.com>
2023-10-20 08:16:55 +0530

    get products route

--- index.js
+++ index.js
@@ -30,40 +30,47 @@ const productSchema = mongoose.Schema({
 });
 
 // create Collection in mongoDB 
-const Product = mongoose.model('product',productSchema);
+const Product = mongoose.model('product', productSchema);
 
-server.post(api_url+"/products",(req,res)=>{
-   const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    price: req.body.price,
-    stock: req.body.stock
-   });
 
-//    save to database 
-product.save().then((created)=>{
-    res.status(201).json(created);
-}).catch((error)=>{
-    res.status(500).json({
-        error: error,
-        success: false
+//====================  routes
+server.post(api_url + "/products", (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        price: req.body.price,
+        stock: req.body.stock
+    });
+
+    //    save to database 
+    product.save().then((created) => {
+        res.status(201).json(created);
+    }).catch((error) => {
+        res.status(500).json({
+            error: error,
+            success: false
+        });
     });
 });
-});
 
+
+server.get(api_url+"/products",async (req,res)=>{
+    const products = await Product.find();
+    res.send(products);
+})
 
 
 
 // connect with database 
-mongoose.connect(process.env.CONNECTION_STRING,{
-    dbName: "nodeapidb" 
+mongoose.connect(process.env.CONNECTION_STRING, {
+    dbName: "nodeapidb"
 })
-.then(()=>{
-    console.log("MongoDB Connected!");
-}).catch((error)=>{
-    console.log(error);
-})
+    .then(() => {
+        console.log("MongoDB Connected!");
+    }).catch((error) => {
+        console.log(error);
+    })
 
-server.listen(port,()=>{
+server.listen(port, () => {
     console.log(`server is running http://localhost:${port}${api_url}`);
 });
