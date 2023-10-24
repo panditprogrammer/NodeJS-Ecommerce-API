c7a4591e42d94f344b5ee90a32c4f4d7c8ccaf0d
panditprogrammer <panditprogrammer@gmail.com>
2023-10-24 22:18:02 +0530

    public api routes for access

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,9 +1,12 @@
 var { expressjwt: jwt } = require("express-jwt");
 
 
-function authJwt(){
+function authJwt() {
     return jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }).unless({
         path: [
+            {
+                url: "/products/", methods: ["GET", "OPTIONS"]
+            },
             "/users/login",
             "/users/register"
         ]
