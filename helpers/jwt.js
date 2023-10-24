ef0fec185e4253e5376c3dea357e422ed28e7c21
panditprogrammer <panditprogrammer@gmail.com>
2023-10-24 22:30:00 +0530

    expose api routes for unauthorized access

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -5,7 +5,8 @@ function authJwt() {
     return jwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }).unless({
         path: [
             {
-                url: "/products/", methods: ["GET", "OPTIONS"]
+                url: /\/products(.*)/, methods: ["GET", "OPTIONS"],
+                url: /\/categories(.*)/, methods: ["GET", "OPTIONS"],
             },
             "/users/login",
             "/users/register"
